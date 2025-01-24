63278bb3ba9b561e4fb1b41e203abb895c0a6d81
Spooro <52739706+alex-rublevsky@users.noreply.github.com>
2025-01-24 18:24:27 -0500

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <h2 className="text-center text-2xl font-bold">
-        Hi, my name is Kristina and I'm CEO of bebra and founder of swag 2012
+        Hi, my name is Kristina and I&apos;m CEO of bebra and founder of swag
+        2012
       </h2>
     </div>
   );
