71187f70090ce36bd984d7f100007e1885d71c71
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-04-27 21:02:17 -0400

    deleted experience section with glowing stroke effect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import FooterContact from "~/components/footerContact";
 import { Gallery } from "~/components/layout-grid";
-import { experienceData } from "~/data/experience";
 import AbalychWorldwide from "~/components/abalychWorldwide";
 import SmoothScroll from "~/components/smoothScroll";
-import { GlowingEffect } from "~/components/ui/glowing-effect";
 import { photographyData, experimentData } from "~/data/gallery";
 import ExperienceSection from "~/components/experienceSection";
 
@@ -29,14 +27,7 @@ function Home() {
             swag 2012
           </h2>
         </section>
-        <section className="flex flex-col gap-6 max-w-screen-2xl mx-auto">
-          <h1 className="mb-6">Professional experience</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            {experienceData.map((entry) => (
-              <div key={entry.title}>{experienceEntry(entry)}</div>
-            ))}
-          </div>
-        </section>
+
         <ExperienceSection />
         <Gallery data={photographyData} title="Photography" />
         <Gallery data={experimentData} title="Experiments" />
@@ -44,54 +35,5 @@ function Home() {
         <FooterContact />
       </main>
     </SmoothScroll>
-  );
-}
-
-function experienceEntry({
-  date,
-  title,
-  company,
-  description,
-  logo,
-  largeLogo,
-}: {
-  date: string;
-  title: string;
-  company: string;
-  description?: string;
-  logo?: string;
-  largeLogo?: boolean;
-}) {
-  return (
-    <div className="relative max-w-[70ch] list-none">
-      <div className="relative h-full rounded-2xl border md:rounded-3xl md:p-3">
-        <GlowingEffect
-          blur={0}
-          borderWidth={3}
-          spread={80}
-          glow={true}
-          disabled={false}
-          proximity={64}
-          inactiveZone={0.01}
-        />
-        <div className="relative flex flex-col items-start gap-4 p-6 overflow-hidden rounded-xl dark:shadow-[0px_0px_27px_0px_#2D2D2D]">
-          {logo && (
-            <img
-              src={`https://pub-0cf7b6988eb140f288f8db5d275ea3b6.r2.dev/${logo}`}
-              className={`w-auto object-contain ${largeLogo ? "h-32" : "h-18"}`}
-              alt={`${company} logo`}
-            />
-          )}
-          <p className="text-muted-foreground">{date}</p>
-          <h5 className="-tracking-4 pt-0.5 font-sans text-xl/[1.375rem] font-semibold text-balance md:text-2xl/[1.875rem]">
-            {title}
-          </h5>
-          <p className="text-base font-medium">{company}</p>
-          {description && (
-            <p className="text-muted-foreground">{description}</p>
-          )}
-        </div>
-      </div>
-    </div>
   );
 }
