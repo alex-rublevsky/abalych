fd657d1db8db66ff07bf066ec605aeae8d648ac3
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-05-29 11:34:45 -0400

    Refactor HeroSection for improved layout and responsiveness

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -9,34 +9,37 @@ export default function HeroSection({ isMobile }: HeroProps) {
   return (
     <section className="relative h-[100dvh] w-screen overflow-x-hidden no-padding no-padding-bottom z-20">
       <div className="max-w-[120rem] mx-auto w-full h-full flex flex-col">
-        <div className="relative w-full flex flex-col justify-center items-center pt-24 ">
-          <div className="flex flex-col items-center">
+        <div className="relative w-full flex flex-col justify-center items-center pt-24 flex-1 md:flex-none px-4">
+          <div className="flex flex-col items-center w-fit">
             <ScatterText
               text="Kristina   Abalymova"
-              className="text-pretty font-bold"
+              className="text-pretty mb-4 text-center"
               extraLarge={true}
+              style={{ maxWidth: isMobile ? "9ch" : "100%" }}
             />
             <ScatterText
               text="aka Abalych"
-              className="text-pretty font-bold text-gray-900 text-sm md:text-lg self-end -mt-2"
+              className="text-pretty self-end whitespace-nowrap"
               extraLarge={false}
             />
           </div>
         </div>
 
-        <div className="relative w-fit mx-auto flex-1 max-h-[calc(100dvh-12rem)]">
+        <div className="relative w-fit mx-auto flex-1 flex flex-col justify-end">
+          {/* TODO: fix the small symbl on small height large width devices */}
           <SymbolA
-            className="absolute left-0 bottom-10 -rotate-25 h-[65vw] max-h-[60dvh] z-[999] mix-blend-difference bg-blend-difference"
+            className="absolute left-0 bottom-10 -rotate-25 h-[65vw] max-h-[60dvh] xl:max-h-[66dvh] z-[999] mix-blend-difference bg-blend-difference"
             color="#ffffff"
+            layoutId="hero-symbol-a"
           />
           <SymbolA
-            className="absolute left-0 bottom-10 -rotate-25 h-[65vw] max-h-[60vh] -z-[1]"
+            className="absolute left-0 bottom-10 -rotate-25 h-[65vw] max-h-[60dvh] xl:max-h-[66dvh] -z-[1]"
             color="#ffffff"
           />
           <img
             src="https://assets.abaly.ch/hero.webp"
             alt="Kristina's portrait"
-            className="w-full h-full object-contain z-3"
+            className="w-full h-full object-contain object-bottom z-3"
           />
         </div>
 
